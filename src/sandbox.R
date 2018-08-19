42106a646f6899d58197c9fd03e3ec4570e5ee35
Li Wang <wangfan8@gmail.com>
2018-08-19 17:18:19 -0400

    add some analysis with respect to authors

--- src/sandbox.R
+++ src/sandbox.R
@@ -6,6 +6,18 @@ library(stringr)
 
 data <- read_csv("~/Dropbox/douban/data/li_douban_book.csv")
 
+# clean up some common writer names
+data %>% 
+  mutate(author = if_else(grepl("村上春树"
+                                , author)
+                          , "Haruki Murakami"
+                          , author)) %>%
+  mutate(author = if_else(grepl("凡尔纳"
+                                , author)
+                          , "Jules Verne"
+                          , author)) ->
+  data
+
 # filter out all chinese books (at least title contains chinese)
 data$chinese_ind <- 0
 data[grep("[\u4e00-\u9fa5]", data$title),]$chinese_ind <- 1
@@ -53,6 +65,36 @@ data %>%
              , group = read_year)) +
   geom_boxplot() +
   facet_grid(col = vars(chinese_ind))
+
+# top 20 authors
+data %>% 
+  filter(status == 'read' & !is.na(author)) %>%
+  group_by(author) %>%
+  tally() %>%
+  top_n(20) %>%
+  mutate(author = reorder(author, n)) %>%
+  ggplot(aes(x = author, y = n)) +
+  geom_col() +
+  coord_flip() +
+  theme_grey(base_family = "STKaiti")
+
+# top 20 authors page distribution
+data %>% 
+  filter(status == 'read' & !is.na(author)) %>%
+  group_by(author) %>%
+  tally() %>%
+  top_n(20) %>%
+  mutate(author = reorder(author, n)) %>%
+  mutate(author = as.character(author)) %>%
+  left_join(data, by = "author") %>%
+  filter(!is.na(pages) & pages > 0) -> data_fav_author
+
+data_fav_author %>%
+  group_by(author)%>%
+  ggplot(aes(x = author, y = pages, group = author)) +
+  geom_boxplot() +
+  coord_flip() +
+  theme_grey(base_family = "STKaiti")
 
 
 
